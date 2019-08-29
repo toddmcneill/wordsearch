057300c798427c149dca46d25992be33716533ae
Todd McNeill <toddmcneill@gmail.com>
2019-08-29 01:18:32 -0600

    Outline first draft algorithm with comments.

--- search.js
+++ search.js
@@ -8,5 +8,25 @@
  * Returns: an array of words that can be found in the word search
  ***/
 module.exports = function search (grid, wordlist) {
+  // Search for one word at a time.
 
+    // Search the grid for the starting letter.
+
+      // Search in each direction around the starting letter for the second letter.
+
+        // If the second letter is found continue in that same direction.
+
+        // If the edge is hit, or if the next letter doesn't match, go back to the starting letter.
+
+        // If the full word is found, add it to the list of found words and move to the next word.
+
+      // If no second letter is found, continue searching the rest of the grid.
+
+    // If the end of the grid is reached, move on to the next word.
+
+  // Return the list of found words.
 }
+
+// Possible optimizations:
+// - If the word is longer than the length and width of the grid, skip it.
+// - Explore recursive vs procedural.
