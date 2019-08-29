e177c6b9dc187db6925fec5338f2ce1c9806d042
Todd McNeill <toddmcneill@gmail.com>
2019-08-29 00:48:35 -0600

    Initial files given in coding assignment

---
+++ search.js
@@ -0,0 +1,12 @@
+/***
+ * search (grid, wordlist)
+ * This function accepts a grid (a 2d array of letters)
+ * and a wordlist (an array of words to search for). The function finds any words
+ * that exist in the word search in any of the 8 possible directions (up, down, left, right
+ * and all 4 diagonal directions). This function is case-insensitive in its matching.
+ *
+ * Returns: an array of words that can be found in the word search
+ ***/
+module.exports = function search (grid, wordlist) {
+
+}
